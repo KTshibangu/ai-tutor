5470e748ed757ee7141c60ffa8663147a4a38461
KTshibangu <ktshibangu@gmail.com>
2026-07-21 16:00:02 +0200

    Made Sidebar and Tobar responsive

--- frontend/src/components/layout/Topbar.jsx
+++ frontend/src/components/layout/Topbar.jsx
@@ -1,37 +1,45 @@
-import { Bell } from "lucide-react";
+import { Bell, Menu } from "lucide-react";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { useAuth } from "../../context/AuthContext";
 
-export default function Topbar() {
+export default function Topbar({ onMenuClick }) {
   const { user } = useAuth();
 
   return (
-    <header className="bg-white border-b px-8 py-4 flex justify-between items-center">
+    <header className="bg-white border-b px-4 md:px-8 py-4 flex justify-between items-center">
 
-      <div>
+      <div className="flex items-center gap-4">
 
-        <h2 className="text-2xl font-bold">
-          Welcome Back
-        </h2>
+        {/* Mobile Menu Button */}
+        <button
+          onClick={onMenuClick}
+          className="lg:hidden"
+        >
+          <Menu size={26} />
+        </button>
 
-        <p className="text-gray-500">
-          {
-            user?.role === "Teacher" ? "Provide Study Materials" : "Ready to continue learning?"
-          }
-        </p>
+        <div>
+          <h2 className="text-xl md:text-2xl font-bold">
+            Welcome Back
+          </h2>
+
+          <p className="text-sm md:text-base text-gray-500">
+            {user?.role === "Teacher"
+              ? "Provide Study Materials"
+              : "Ready to continue learning?"}
+          </p>
+        </div>
 
       </div>
 
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-4 md:gap-6">
 
         <Bell className="cursor-pointer" />
 
         <Avatar>
-
           <AvatarFallback>
             {user?.username?.charAt(0).toUpperCase()}
           </AvatarFallback>
-
         </Avatar>
 
       </div>
